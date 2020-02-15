18febd2f42f82fc04690cb05cd2b6532fd43369d
howlcipher <wylelias.123@gmail.com>
2020-02-15 23:42:30 +0000

    It now displays the difference as well

--- main.py
+++ main.py
@@ -9,9 +9,10 @@ second_Name = str(input("Type the second name: "))
 def compare_Names(f, s):
     #first name is longer
     if len(f) > len(s):
+        dif = len(f) - len(s)
         print(
-            "{}'s name is longer.  {} is {} character(s) long, while {} is only {} character(s) long."
-            .format(f, f, len(f), s, len(s)))
+            "{}'s name is longer.  {} is {} character(s) long, while {} is only {} character(s) long.  With a difference of {}."
+            .format(f, f, len(f), s, len(s), dif))
 
     #the names are the same length
     elif len(f) == len(s):
@@ -21,9 +22,10 @@ def compare_Names(f, s):
 
     #second name is longer
     else:
+        dif = len(s) - len(f)
         print(
-            "{}'s name is longer.  {} is {} character(s) long, while {} is only {} characters long."
-            .format(s, s, len(s), f, len(f)))
+            "{}'s name is longer.  {} is {} character(s) long, while {} is only {} characters long.  With a difference of {}."
+            .format(s, s, len(s), f, len(f), dif))
 
 
 #takes two strings and compares them by length, displays their character count, and which is the longer
